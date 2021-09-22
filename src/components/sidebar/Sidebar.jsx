3da9a68adc78e8e43687aa5b3dc8508dceb90fc9
MagicianAD <amazingaarati@gmail.com>
2021-09-23 00:13:07 +0545

    second commit

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,25 +1,76 @@
 import React from "react";
 import "./sidebar.css";
-import { LineStyle, Timeline, TrendingUp } from "@material-ui/icons";
+import {
+  LineStyle,
+  Timeline,
+  TrendingUp,
+  PersonOutline,
+  SignalCellularAlt,
+  CardTravel,
+  AttachMoney,
+  MailOutline,
+  DynamicFeed,
+  ChatBubbleOutline,
+} from "@material-ui/icons";
 
 function sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebarWrapper">
         <div className="sidebarMenu">
-          <h3 className="sidebarlist">Dashboard</h3>
-          <li className="sideListItems">
-            <LineStyle />
-            home
-          </li>
-          <li className="sideListItems">
-            <Timeline />
-            Analytics
-          </li>
-          <li className="sideListItems">
-            <TrendingUp />
-            Sales
-          </li>
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem active">
+              <LineStyle className="sidebarIcon" />
+              Home
+            </li>
+            <li className="sidebarListItem ">
+              <Timeline />
+              Analytics
+            </li>
+            <li className="sidebarListItem ">
+              <TrendingUp />
+              Sales
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Quick menu</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem ">
+              <PersonOutline />
+              User
+            </li>
+            <li className="sidebarListItem ">
+              <CardTravel />
+              Production
+            </li>
+            <li className="sidebarListItem ">
+              <SignalCellularAlt />
+              Report
+            </li>
+            <li className="sidebarListItem ">
+              <AttachMoney />
+              Transaction
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Notification</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem ">
+              <MailOutline />
+              Mail
+            </li>
+            <li className="sidebarListItem ">
+              <DynamicFeed />
+              Feedback
+            </li>
+            <li className="sidebarListItem ">
+              <ChatBubbleOutline />
+              Message
+            </li>
+          </ul>
         </div>
       </div>
     </div>
